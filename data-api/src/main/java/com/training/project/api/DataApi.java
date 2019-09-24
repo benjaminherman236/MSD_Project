a2f89115cbba720707d00c1541b723b86b1ca94e
user <user@this.com>
2019-09-24 16:04:17 -0400

    Sprint 1 day 2

---
+++ data-api/src/main/java/com/training/project/api/DataApi.java
@@ -0,0 +1,11 @@
+package com.training.project.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApi {
+public static void main(String[] args) {
+	SpringApplication.run(DataApi.class, args);
+}
+}
---
+++ data-api/src/main/java/com/training/project/api/data/EventResource.java
@@ -0,0 +1,15 @@
+package com.training.project.api.data;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class EventResource {
+	@GetMapping("/events")
+	public CustomerResponse getCustomer() {
+		return new CustomerResponse("Bob", "bob@bob.com", "password");
+	}
+
+}

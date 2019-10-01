4ebd6313284e32362ea2fe01c198e8c1ba826bf8
user <user@this.com>
2019-10-01 13:32:45 -0400

    added token file

---
+++ data-api/src/main/java/com/training/project/auth/JWTDataHelper.java
@@ -0,0 +1,14 @@
+/*
+ * package com.training.project.auth;
+ * 
+ * 
+ * public class JWTDataHelper implements JWTDataUtil{
+ * 
+ * @Override public String getScopes(String jwt_token) { // TODO Auto-generated
+ * method stub return null; }
+ * 
+ * @Override public Token createToken(String scopes) { // TODO Auto-generated
+ * method stub return null; }
+ * 
+ * }
+ */
